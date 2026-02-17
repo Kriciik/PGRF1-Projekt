e2f7bac5092be3e9a622fa130edfd2a1239ef7bd
Jiří Křička <61604360+Kriciik@users.noreply.github.com>
2026-02-17 13:08:31 +0100

    Add files via upload

---
+++ src/model/Vertex.java
@@ -0,0 +1,37 @@
+package model;
+
+import transforms.Col;
+import transforms.Point3D;
+
+public class Vertex {
+    private final Point3D position;
+    private final Col color;
+
+    public Vertex(double x, double y, double z) {
+        this.position = new Point3D(x,y,z);
+        this.color = new Col(0xfffffff);
+    }
+
+    public Vertex(double x, double y, double z, Col color) {
+        this.position = new Point3D(x,y,z);
+        this.color = color;
+    }
+
+    public Point3D getPosition() {
+        return position;
+    }
+
+    public Col getColor() {
+        return color;
+    }
+
+    public double getX() {
+        return position.getX();
+    }
+    public double getY() {
+        return position.getY();
+    }
+    public double getZ() {
+        return position.getZ();
+    }
+}
